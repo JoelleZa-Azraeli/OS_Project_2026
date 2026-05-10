2e57ae1f7762b059107ee57017c9668afb84e2f5
HiBA-Kl <hibajone31@gmail.com>
2026-05-10 23:57:18 +0300

    Update dijkstra.h

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -3,6 +3,11 @@
 
 #include "graph.h"
 
+// Milestone 1 function
 void find_shortest_path(Graph* g, int start, int end);
+
+// Milestone 3: Shared variables for the GUI animation
+extern int final_path[MAX_NODES];
+extern int final_path_count;
 
 #endif
